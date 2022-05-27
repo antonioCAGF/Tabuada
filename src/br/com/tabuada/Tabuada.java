0b0d47df6c21900e345e4c9da6412d484262976e
Antonio Guimarães <antonioguimaraes2604@gmail.com>
2022-05-27 09:09:46 -0300

    Tabuada do 1 ao 10 desenvolvida com for

---
+++ src/br/com/tabuada/Tabuada.java
@@ -0,0 +1,24 @@
+package br.com.tabuada;
+
+public class Tabuada {
+	public static void main(String[] args) {
+		
+		// Instagram: @pikdev_
+
+		// Youtube: @Pikachu Gamer
+		
+		// GitHub: @antonioCAGF
+		
+		// Linkedin: https://www.linkedin.com/in/antonio-guimar%C3%A3es-007ba61a8
+
+		for (int multiplica = 1; multiplica <= 10; multiplica++) {
+			for (int valor = 0; valor <= 10; valor++) {
+				int total = multiplica * valor;
+				System.out.print(total);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	
+	}
+}
